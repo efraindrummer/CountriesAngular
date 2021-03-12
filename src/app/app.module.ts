60580c0439ea5be16bd22d8c6c6eefb80533391b
Efrain May <efrain_may_75@hotmail.com>
2021-03-12 13:51:39 -0600

    servicio para busqueda de pais mostrando data en consola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     PaisModule,
     SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -23,7 +23,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
   ]
 })
 export class PaisModule { }
