58d99975fad2a57349dc33fff42187ec8d1c838f
Efrain May <efrain_may_75@hotmail.com>
2021-03-13 12:06:36 -0600

    coponente de input y tabla separado - emitioncon Decorators

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -16,11 +16,11 @@ export class PorPaisComponent {
 
   constructor(private paisService: PaisService){}
 
-  buscar(){
+  buscar(termino: string){
     this.hayError = false;
-    console.log(this.termino);
+    this.termino = termino;
     
-    this.paisService.buscarPais(this.termino)
+    this.paisService.buscarPais(termino)
       .subscribe((paises) => {
         console.log(paises);
         
