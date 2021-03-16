95e057e8e1640c6692038cd276f12f85c6f9b759
Efrain May <efrain_may_75@hotmail.com>
2021-03-16 00:42:25 -0600

    por capital

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime, min } from 'rxjs/operators';
 
@@ -13,6 +13,8 @@ export class PaisInputComponent implements OnInit{
   
   @Output() onEnter    :EventEmitter<string> = new EventEmitter();
   @Output() onDebounce :EventEmitter<string> = new EventEmitter();
+
+  @Input() placeholder: string = '';
   
   debouncer: Subject<string> = new Subject();
   
