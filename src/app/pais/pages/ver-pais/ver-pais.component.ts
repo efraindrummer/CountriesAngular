69c487b778b8bec856cc259a6b95b537eaec70da
Efrain May <efrain_may_75@hotmail.com>
2021-03-16 10:36:07 -0600

    operador switchMap para optimizacion del params + diseño de ver-pais-component

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,16 +12,19 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+
   constructor(private activatedRoute: ActivatedRoute,private paisService: PaisService) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe( ({id}) => {
-      console.log(id);
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
+      )
+      .subscribe(pais => this.pais = pais);
+    
 
-      this.paisService.getPaisPorAlpha(id).subscribe(pais => {
-        console.log(pais);
-      })
-    });
   }
 
 }
